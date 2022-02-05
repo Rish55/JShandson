ec3cfe6bccb8142c64952ccebee6213df5f3998a
Rishabh Gautam <rishabhgautam@cedcoss.com>
2022-02-05 10:54:25 +0530

    Initializing Variables with the Assignment Operator

--- src/JShandson.js
+++ src/JShandson.js
@@ -1,12 +1,1 @@
-//This is my SINGLE LINE comment
-/*This is my
-MULTILINE comment*/
-var myName;
-
-// Setup
-var a;
-// Only change code below this line
-a=7;
-var b;
-// Only change code below this line
-b=a;
+var a = 9;
