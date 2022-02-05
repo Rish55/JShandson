565f5259097193863193498bca0eb381ea55159d
Rishabh Gautam <rishabhgautam@cedcoss.com>
2022-02-05 14:47:47 +0530

    Stand in Line

--- src/JShandson.js
+++ src/JShandson.js
@@ -1,18 +1,17 @@
-// Example
-var changed = 0;
-
-function change(num) {
-  return (num + 5) / 3;
-}
-
-changed = change(10);
-
-// Setup
-var processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
-processed = processArg(7);
+function nextInLine(arr, item) {
+    // Your code here
+  
+    var queue = arr.push(item);
+  
+    var removeItem = arr.shift();
+  
+    return removeItem;  // Change this line
+  }
+  
+  // Test Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display Code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6)); // Modify this line to test
+  console.log("After: " + JSON.stringify(testArr));
