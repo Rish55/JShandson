b56f461f933e055b9d9decebf084fc3788dc9758
Rishabh Gautam <rishabhgautam@cedcoss.com>
2022-02-05 15:29:34 +0530

    Comparison with the Less Than or equal tp Operator

--- src/JShandson.js
+++ src/JShandson.js
@@ -1,8 +1,7 @@
-function testLessThan(val) {
-    if (val < 25) return "Under 25";
+function testLessOrEqual(val) {
+    if (val <= 12) return "Smaller Than or Equal to 12";
   
-    if (val < 55) return "Under 55";
+    if (val <= 24) return "Smaller Than or Equal to 24";
   
-    return "55 or Over";
+    return "More Than 24";
   }
-  
