b1c6f0088033f7ebf3de1d9ec43cd7548a2d98e8
Rishabh Gautam <rishabhgautam@cedcoss.com>
2022-02-05 14:42:28 +0530

    Global vs. Local Scope in Functions

--- src/JShandson.js
+++ src/JShandson.js
@@ -1,11 +1,14 @@
-function myLocalScope() {
-    'use strict';
-  
-    var myVar = "Hello";
-  
-    console.log(myVar);
-  }
-  myLocalScope();
-  
-  // Run and check the console
-  // myVar is not defined outside of myLocalScope
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
